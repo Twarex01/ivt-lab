55c5acdd50d9918415b1aad071c09513ad063b06
Twarex <twarex01@gmail.com>
2020-04-23 19:20:02 +0200

    verify

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -22,23 +22,25 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
-    int test = 0;
+    ship.fireTorpedo(FiringMode.SINGLE);
     // Assert
-    assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(0)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
-
+    ship.fireTorpedo(FiringMode.ALL);
     // Assert
-    assertEquals(true, result);
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(1)).fire(1);
   }
 
 }
