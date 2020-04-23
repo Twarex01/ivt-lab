fada604b464e4cabee7fc6ade33fd12ed482d873
Twarex <twarex01@gmail.com>
2020-04-23 23:28:07 +0200

    final

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -106,5 +106,83 @@ public class GT4500Test {
     verify(secondary, times(0)).fire(1);
   }
 
+  @Test
+  public void test6_Single_Success(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(true);
+    when(secondary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(false);
+    when(secondary.fire(1)).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+    // Assert
+    verify(secondary, times(0)).fire(1);
+    verify(primary, times(1)).fire(1);
+  }
+
+  @Test
+  public void test7_Single_Success(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(secondary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(true);
+    when(secondary.fire(1)).thenReturn(false);
+    // Act
+    ship.fireTorpedo(FiringMode.ALL);
+    // Assert
+    verify(secondary, times(1)).fire(1);
+    verify(primary, times(1)).fire(1);
+  }
+
+  @Test
+  public void test8_Single_Success(){
+    // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(secondary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(true);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    // Assert
+    verify(primary, times(1)).fire(1);
+  }
+
+  @Test
+  public void test9_Single_Success() {
+    // Arrange
+    when(primary.isEmpty()).thenReturn(false);
+    when(secondary.isEmpty()).thenReturn(true);
+    when(primary.fire(1)).thenReturn(true);
+    // Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    //Arrange
+    when(primary.isEmpty()).thenReturn(true);
+    //Act
+    ship.fireTorpedo(FiringMode.SINGLE);
+    //Assert
+    verify(primary, times(1)).fire(1);
+    verify(secondary, times(0)).fire(1);
+  }
+
+  @Test
+  public void fireLaser(){
+    // Arrange
+
+    // Act
+    boolean result = ship.fireLaser(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void switchcase(){
+    // Arrange
+
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.OTHER);
+
+    // Assert
+    assertEquals(false, result);
+  }
 
 }
